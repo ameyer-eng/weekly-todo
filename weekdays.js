abd8bcd45aa26aa907d52275a3b9a10c8effe9f2
ameyer-eng <ameyer.engineering@gmail.com>
2021-06-29 21:01:14 -0500

    Got table working with hours and res input

--- weekdays.js
+++ weekdays.js
@@ -1,14 +1,17 @@
-//Going to attempt to create weekday 2d weekday array
-var workout_list =[
-    ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    ["Monday1", "Tuesday1", "Wednesday1", "Thursday1", "Friday1", "Saturday1", "Sunday1"],
-    ["Monday2", "Tuesday2", "Wednesday2", "Thursday2", "Friday2", "Saturday2", "Sunday2"]
-];
 
-var weekly_weather = ["Sunny 90F", "Sunny 85F", "Rainy 75F","Sunny 90F","Sunny 90F","Sunny 90F","Sunny 90F" ];
 
 function generate_table() {
- //const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+ var current_hour = 0;
+ 
+ var time_delta = parseFloat(document.getElementById("res_text_box").value);
+ var hours = parseFloat(document.getElementById("hour_text_box").value);
+ var num_of_rows = Math.ceil(hours/time_delta); 
+
+ console.log(time_delta);
+ console.log(hours);
+ console.log(num_of_rows);
+
+ const weekdays = ["Hour", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
  // get the reference for the body
   var body = document.getElementsByTagName("body")[0];
  
@@ -17,16 +20,28 @@ function generate_table() {
   var tblBody = document.createElement("tbody");
 
   // creating all cells
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < num_of_rows; i++) {
     // creates a table row
     var row = document.createElement("tr");
 
-    for (var j = 0; j < 7; j++) {
+    for (var j = 0; j < 8; j++) {
       // Create a <td> element and a text node, make the text
       // node the contents of the <td>, and put the <td> at
       // the end of the table row
       var cell = document.createElement("td");
-      		var cellText = document.createTextNode(workout_list[i][j]);
+      if(i <1){
+        var cellText = document.createTextNode(weekdays[j]);
+      }else {
+         
+         if(j < 1){
+          cellText = document.createTextNode(String(current_hour));
+          current_hour = current_hour + time_delta;
+         }
+         else{
+          cellText = document.createTextNode("Blah!!");
+         }
+      }
+      	
 
       cell.appendChild(cellText);
       row.appendChild(cell);
@@ -41,53 +56,13 @@ function generate_table() {
   // appends <table> into <body>
   body.appendChild(tbl);
   // sets the border attribute of tbl to 2;
-  tbl.setAttribute("border", "2");
+  tbl.setAttribute("border", "1");
   tbl.setAttribute("width", "50%");
 }
 
 
-//Adding function to create weather table
-function generate_table_1() {
-    //const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    // get the reference for the body
-     var body = document.getElementsByTagName("body")[0];
-    
-     // creates a <table> element and a <tbody> element
-     var tbl = document.createElement("table");
-     var tblBody = document.createElement("tbody");
-   
-     // creating all cells
-     for (var i = 0; i < 1; i++) {
-       // creates a table row
-       var row = document.createElement("tr");
-   
-       for (var j = 0; j < 7; j++) {
-         // Create a <td> element and a text node, make the text
-         // node the contents of the <td>, and put the <td> at
-         // the end of the table row
-         var cell = document.createElement("td");
-                 var cellText = document.createTextNode(weekly_weather[j]);
-   
-         cell.appendChild(cellText);
-         row.appendChild(cell);
-       }
-   
-       // add the row to the end of the table body
-       tblBody.appendChild(row);
-     }
-   
-     // put the <tbody> in the <table>
-     tbl.appendChild(tblBody);
-     // appends <table> into <body>
-     body.appendChild(tbl);
-     // sets the border attribute of tbl to 2;
-     tbl.setAttribute("border", "2");
-     tbl.setAttribute("width", "50%");
 
-   }
+
    
-   function generate_table_all()
-   {
-       generate_table_1();
-       generate_table();
-   }
+
+ 
